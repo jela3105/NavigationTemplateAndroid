30453d45e2760316933698a43438ba83ca63c868
jela3105 <jela3105@gmail.com>
2020-10-13 23:02:57 -0500

    Bottom navigation and tabs working

---
+++ app/src/main/java/com/example/bottomnavigation/ui/home/HomeFragment.java
@@ -0,0 +1,78 @@
+package com.example.bottomnavigation.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.bottomnavigation.R;
+import com.google.android.material.tabs.TabLayout;
+
+public class HomeFragment extends Fragment {
+
+    View myFragment;
+
+    ViewPager viewPager;
+    TabLayout tabLayout;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    public static HomeFragment getInstance()    {
+        return new HomeFragment();
+    }
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        myFragment = inflater.inflate(R.layout.fragment_home, container, false);
+
+        viewPager = myFragment.findViewById(R.id.viewPager);
+        tabLayout = myFragment.findViewById(R.id.tabLayout);
+
+        return myFragment;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        setUpViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    private void setUpViewPager(ViewPager viewPager) {
+        SectionPagerAdapter adapter = new SectionPagerAdapter(getChildFragmentManager());
+
+        adapter.addFragment(new Tab1Fragment(), "Fixtures");
+        adapter.addFragment(new Tab2Fragment(), "Live");
+        adapter.addFragment(new Tab1Fragment(),"Result");
+
+        viewPager.setAdapter(adapter);
+    }
+}
