2987ecebbe064ad57f31e0ecd95a78be597ce127
jela3105 <jela3105@gmail.com>
2020-11-22 21:12:32 -0600

    Add methods to HomeAdapter

--- app/src/main/java/com/example/spotifyclone/ui/home/HomeAdapter.java
+++ app/src/main/java/com/example/spotifyclone/ui/home/HomeAdapter.java
@@ -1,12 +1,52 @@
 package com.example.spotifyclone.ui.home;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.spotifyclone.ItemMusic;
+import com.example.spotifyclone.R;
 
 import java.util.ArrayList;
 
-public class HomeAdapter {
+public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeViewHolder> {
+
+    ArrayList<ItemMusic> homeList;
 
     public HomeAdapter(ArrayList<ItemMusic> homeList) {
+        this.homeList = homeList;
+    }
 
+    @NonNull
+    @Override
+    public HomeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_image_description, null, false);
+        return new HomeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HomeViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    public class HomeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public HomeViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+        }
     }
 }
