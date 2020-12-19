ae325d3610bf0fb8133df4b92f06395447be2b0b
jela3105 <jela3105@gmail.com>
2020-12-18 19:42:19 -0600

    Finish ItemMusicBigAdapter

--- app/src/main/java/com/example/spotifyclone/adapters/ItemMusicBigAdapter.java
+++ app/src/main/java/com/example/spotifyclone/adapters/ItemMusicBigAdapter.java
@@ -1,4 +1,53 @@
 package com.example.spotifyclone.adapters;
 
-public class ItemMusicBigAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.spotifyclone.R;
+import com.example.spotifyclone.items.ItemMusic;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class ItemMusicBigAdapter extends RecyclerView.Adapter<ItemMusicBigAdapter.ItemMusicBigViewHolder>{
+    ArrayList<ItemMusic> musicList;
+    @NonNull
+    @Override
+    public ItemMusicBigViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_music_big, null, false);
+        return new ItemMusicBigViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemMusicBigViewHolder holder, int position) {
+        Picasso.with(holder.context).load(musicList.get(position).getImage()).into(holder.image);
+        holder.title.setText(musicList.get(position).getTitle());
+        holder.description.setText(musicList.get(position).getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return musicList.size();
+    }
+
+    public class ItemMusicBigViewHolder extends RecyclerView.ViewHolder {
+        Context context;
+        TextView title;
+        TextView description;
+        ImageView image;
+        public ItemMusicBigViewHolder(@NonNull View itemView) {
+            super(itemView);
+            context = itemView.getContext();
+            title = itemView.findViewById(R.id.item_music_big_title);
+            description = itemView.findViewById(R.id.item_music_big_description);
+            image = itemView.findViewById(R.id.item_music_big_image);
+        }
+    }
 }
